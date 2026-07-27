9c3e024a4c2f89a9e60c14d9321ca90487ffa5d6
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-07-27 20:37:11 +0530

    Time: 35 ms (58.55%), Space: 13.4 MB (29.49%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums) - 1
+        maxReach = 0
+        for i in range(len(nums)):
+            if i > maxReach:
+                return False
+            maxReach = max(maxReach,i + nums[i])
+            if maxReach >= n:
+                return True
+        return False
