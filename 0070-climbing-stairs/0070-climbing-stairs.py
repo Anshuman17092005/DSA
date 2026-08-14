0ad62ff49a0dc1f50677f1090ee235fe885581f4
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-08-15 00:01:32 +0530

    Time: 0 ms (100%), Space: 12.2 MB (90.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return n
+        prev2 = 1
+        prev = 2
+        for i in range(3,n+1):
+            curr = prev2 + prev
+            prev2 = prev
+            prev = curr
+        return prev
