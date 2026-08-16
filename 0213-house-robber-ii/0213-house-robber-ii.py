b1fa2f8497f5474177a047b1b7882c2447dbddc5
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-08-16 16:02:06 +0530

    Time: 0 ms (100%), Space: 12.4 MB (23.25%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        def case(arr):
+            n = len(arr)
+            prev2 = 0
+            prev = 0
+            for money in arr:
+                curr = max(prev,money+prev2)
+                prev2 = prev
+                prev = curr
+            return prev
+        case1 = case(nums[:-1])
+        case2 = case(nums[1:])
+        return max(case1,case2)
