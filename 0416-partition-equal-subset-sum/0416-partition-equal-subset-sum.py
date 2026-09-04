1db065e3445f0a95695117773e850f13d12b98f9
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-04 23:17:05 +0530

    Time: 816 ms (71.2%), Space: 13 MB (51.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,20 +1,13 @@
 class Solution:
-  def canPartition(self,num):
-    total = sum(num)
+  def canPartition(self,nums):
+    total = sum(nums)
     if total % 2 != 0:
       return False
     target = total // 2
-    dp = [[False] * (target + 1) for _ in range(len(num)+1)]
-    def solve(arr,target,dp):
-      n = len(arr)
-      for i in range(n+1):
-        dp[i][0] = True
-      for i in range(n-1,-1,-1):
-        for t in range(1,target + 1):
-          take = False
-          if arr[i] <= t:
-            take = dp[i+1][t-arr[i]]
-          notTake = dp[i+1][t]
-          dp[i][t] = notTake or take
-      return dp[0][target]
-    return solve(num,target,dp)
+    dp = [False] * (target+1)
+    dp[0] = True
+    for num in nums:
+      for t in range(target,num-1,-1):
+        dp[t] = dp[t] or dp[t-num]
+    return dp[target]
+    
