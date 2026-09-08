d726696250a8b3c182c4a67e6dfd1cf78ee81271
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-08 07:00:35 +0530

    Time: 0 ms (100%), Space: 12.4 MB (22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
         n = len(nums)
-        prev2 = nums[0]
-        prev = max(nums[0],nums[1])
+        if n == 1:
+            return nums[0]
+        dp = [-1]*(n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
         for i in range(2,n):
-            curr = max(prev,nums[i]+prev2)
-            prev2 = prev
-            prev = curr
-        return prev
+            take = nums[i] + dp[i-2]
+            notTake = dp[i-1]
+            dp[i] = max(take,notTake)
+        return dp[n-1]
