908c2652b5e61299fc2703a80c69c9c2c5ab4fd4
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-16 14:37:11 +0530

    Time: 40 ms (35.69%), Space: 19.4 MB (96.27%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -12,7 +12,7 @@ class Solution(object):
         """
         slow = head
         fast = head
-        while fast is not None and fast.next is not None:
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
