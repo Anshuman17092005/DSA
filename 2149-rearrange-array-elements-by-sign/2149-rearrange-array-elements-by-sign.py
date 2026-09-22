739d189d6d1867739c718608d8b1d39e7b8e3b84
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-22 11:28:22 +0530

    Time: 70 ms (66.71%), Space: 47.9 MB (24.83%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        result = [0] * (len(nums))
         pos = 0
         neg = 1
-        n = len(nums)
-        result = [0] * n
-        for num in nums:
-            if num> 0:
-                result[pos] = num
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result[pos] = nums[i]
                 pos += 2
             else:
-                result[neg] = num
+                result[neg] = nums[i]
                 neg += 2
         return result
