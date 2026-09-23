9cada7fc1ace3f8f903019f97aa7751e076f27e1
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-23 14:05:10 +0530

    Time: 0 ms (100%), Space: 12.9 MB (31.57%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,14 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left = 0
-        right = len(nums)-1
-        while left <= right:
-            mid = (left+right)//2
+        low = 0
+        high = len(nums)-1
+        while low <= high:
+            mid = (low+high)//2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                left = mid+1
+                low = mid+1
             else:
-                right = mid-1
-        return left
+                high = mid-1
+        return low
