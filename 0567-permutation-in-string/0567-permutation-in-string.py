58aaeb9811fa60fe6a41dc41261788328a0c301b
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-23 10:03:16 +0530

    Time: 32 ms (36.43%), Space: 12.9 MB (19.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -5,15 +5,14 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        left = 0
         freq1 = {}
-        for i in range(len(s1)):
-            freq1[s1[i]] = freq1.get(s1[i],0)+1
         freq2 = {}
+        for i in s1:
+            freq1[i] = freq1.get(i,0)+1
         left = 0
         for right in range(len(s2)):
-            freq2[s2[right]] = freq2.get(s2[right],0) + 1
-            while (right-left+1) > len(s1):
+            freq2[s2[right]] = freq2.get(s2[right],0)+1
+            while right-left+1 > len(s1):
                 freq2[s2[left]] -= 1
                 if freq2[s2[left]] == 0:
                     del freq2[s2[left]]
