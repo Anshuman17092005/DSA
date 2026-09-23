2a4adfe20161eb90dec8e33ceb5f2b4413b81af6
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-23 12:15:16 +0530

    Time: 121 ms (79.76%), Space: 66.7 MB (46.98%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -13,17 +13,16 @@ class Solution(object):
         fast = head.next
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next
-        second = slow.next
+            fast =fast.next.next
+        temp = slow.next
         slow.next = None
         prev = None
-        temp = second
         while temp:
             front = temp.next
             temp.next = prev
             prev = temp
             temp = front
-        while prev:
+        while head and prev:
             if head.val != prev.val:
                 return False
             head = head.next
