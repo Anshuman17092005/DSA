a8ff5562e28237fccb9985b87c8daa0b5fc54007
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-23 10:30:47 +0530

    Time: 215 ms (17.75%), Space: 15.7 MB (24.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -5,27 +5,25 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        if s == t:
-            return s
         freq1 = {}
-        for i in range(len(t)):
-            freq1[t[i]] = freq1.get(t[i],0)+1
+        for i in t:
+            freq1[i] = freq1.get(i,0)+1
+        left = 0
         freq2 = {}
         result = ""
-        left = 0
-        required = len(freq1)
         mini = float('inf')
+        required = len(freq1)
         formed = 0
         for right in range(len(s)):
             freq2[s[right]] = freq2.get(s[right],0)+1
             if s[right] in freq1 and freq1[s[right]] == freq2[s[right]]:
                 formed += 1
             while formed == required:
-                if right - left + 1 < mini:
-                    mini = right-left+1
+                if right-left+1<mini:
+                    mini = right-left + 1
                     result = s[left:right+1]
                 freq2[s[left]] -= 1
-                if s[left] in freq1 and freq1[s[left]] > freq2[s[left]]:
+                if s[left] in freq1 and freq2[s[left]] < freq1[s[left]]:
                     formed -= 1
                 left += 1
         return result
