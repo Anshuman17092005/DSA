4d83c0d7d5325dd2ad611b7ce9bb3100267951d7
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-26 17:08:33 +0530

    Time: 3 ms (18.57%), Space: 12.4 MB (20.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,10 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
         low = 0
-        high = n -1
         mid = 0
+        high = len(nums)-1
         while mid <= high:
             if nums[mid] == 0:
                 nums[low],nums[mid] = nums[mid],nums[low]
