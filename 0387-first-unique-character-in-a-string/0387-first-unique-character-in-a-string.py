26c459958fc3f026e294197f4f4fa04a89daf94a
Anshuman17092005 <anshuman.khandelwal.ecs.2023@nist.edu>
2026-09-26 14:45:46 +0530

    Time: 136 ms (17.81%), Space: 16 MB (5.12%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -5,8 +5,8 @@ class Solution(object):
         :rtype: int
         """
         freq = {}
-        for i in s:
-            freq[i] = freq.get(i,0) + 1
+        for i in range(len(s)):
+            freq[s[i]] = freq.get(s[i],0)+1
         for i in range(len(s)):
             if freq[s[i]] == 1:
                 return i
